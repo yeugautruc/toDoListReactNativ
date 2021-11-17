edb9381d1fbe2fc05682af3089ecf77067f05bf5
yeugautruc <yeugautruc@gmail.com>
2021-11-17 11:14:11 +0100

    navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LandingScreen } from './src/screens/Landingscreen';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { HomeScreen } from './src/screens/HomeScreen';
+
+const switchNavigator = createSwitchNavigator({
+  landingStack: {
+    screen: createStackNavigator({
+      Landing: LandingScreen,
+    }, {
+      defaultNavigationOptions: {
+        headerShown: false
+      }
+    })
+  },
+  homeStack: createBottomTabNavigator({
+    // Home Tab Icon
+    home: {
+      screen: createStackNavigator({
+        Homepage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_icon_n.png')
+          return <img src={icon} style={styles.tabIcon}></img>
+        }
+      }
+    },
+
+    // Offer Tab Icon
+    Offer: {
+      screen: createStackNavigator({
+        Homepage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_icon_n.png')
+          return <img src={icon} style={styles.tabIcon}></img>
+        }
+      }
+    },
+
+    // Cart Tab Icon
+    Cart: {
+      screen: createStackNavigator({
+        Homepage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_icon_n.png')
+          return <img src={icon} style={styles.tabIcon}></img>
+        }
+      }
+    },
+
+    // Account Tab Icon
+    Account: {
+      screen: createStackNavigator({
+        Homepage: HomeScreen
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon = focused == true ? require('./src/images/home_icon.png') : require('./src/images/home_icon_n.png')
+          return <img src={icon} style={styles.tabIcon}></img>
+        }
+      }
+    }
+  })
+})
+
+const AppNavigation = createAppContainer(switchNavigator)
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigation />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tabIcon: {
+    width: 30,
+    height: 30
   },
 });
---
+++ src/screens/Landingscreen.tsx
@@ -0,0 +1,101 @@
+import React from "react"
+import { useState, useReducer, useEffect } from "react";
+import { View, Text, StyleSheet, Dimensions, Image, Alert } from 'react-native'
+import * as Location from 'expo-location'
+
+const screenWidth = Dimensions.get('screen').width
+
+export const LandingScreen = () => {
+
+    const [errorMsg, setErrMsg] = useState("")
+    const [address, setAddress] = useState<Location.LocationGeocodedAddress>()
+    const [displayAddress, setDisplayAddress] = useState("Waiting for your current location")
+
+    useEffect(() => {
+        (async () => {
+            Alert.alert('hier')
+            let { status } = await Location.requestBackgroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrMsg('Permission to access location is not granted')
+            }
+            let location: any = await Location.getCurrentPositionAsync({});
+            const { coords } = location
+            if (coords) {
+                const { latitude, longitude } = coords;
+                let addressResponse: any = await Location.reverseGeocodeAsync({ latitude, longitude })
+
+                for (let item of addressResponse) {
+                    setAddress(item)
+                    let currentAddress = `${item.name},${item.street},${item.postalCode},${item.country}`
+                    setDisplayAddress(currentAddress)
+                    return;
+                }
+            } else {
+                // notify location is wrong
+            }
+        })
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.navigation} />
+
+            <View style={styles.body}>
+                <Image source={require('../images/location_icon.png')} style={styles.location_icon} />
+                <View style={styles.addressContainer}>
+                    <Text style={styles.addressTitle}>
+                        Your Address
+                    </Text>
+                </View>
+
+                <Text style={styles.addressText}>
+                    {displayAddress}
+                </Text>
+            </View>
+
+            <View style={styles.footer} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(242,242,242,1)'
+    },
+    navigation: {
+        flex: 2,
+    },
+    body: {
+        flex: 9,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: 'cyan'
+    },
+    location_icon: {
+        width: 80,
+        height: 80
+    },
+    addressContainer: {
+        width: screenWidth - 100,
+        borderBottomColor: 'red',
+        borderBottomWidth: 0.5,
+        padding: 5,
+        marginBottom: 10,
+        alignItems: 'center'
+    },
+    addressTitle: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: '#7D7D7D'
+    },
+    addressText: {
+        fontSize: 14,
+        fontWeight: '400',
+        color: '#4F4F4F'
+    }
+
+})
